ebf54eee9bd463e0a8bcdc8371f109d9bdc4b369
Roberto Guanciale <robertog@kth.se>
2017-02-13 16:37:02 +0100

    Minimal Othello Backend service

---
+++ othello-service/src/se/kth/swacademy/othelloservice/JsonTransformer.java
@@ -0,0 +1,14 @@
+package se.kth.swacademy.othelloservice;
+
+import com.google.gson.Gson;
+
+import spark.ResponseTransformer;
+
+public class JsonTransformer implements ResponseTransformer {
+	private Gson gson = new Gson();
+
+	public String render(Object model) throws Exception {
+		return gson.toJson(model);
+	}
+
+}
---
+++ othello-service/src/se/kth/swacademy/othelloservice/MatchResult.java
@@ -0,0 +1,7 @@
+package se.kth.swacademy.othelloservice;
+
+public class MatchResult {
+	public String winner;
+    public int duration;
+    public int moves;
+}
